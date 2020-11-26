4700680fbd42a4c9eece8a63e66874ca3dcee321
fdsa654hg <fdsa654hg@gmail.com>
2020-11-26 18:22:49 +0800

    Add eng2chi dataset class and modify other class for it

---
+++ s2s/dset/_eng2chi.py
@@ -0,0 +1,33 @@
+import os
+
+from typing import Sequence
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+from s2s.dset._base import BaseDset
+from s2s.path import DATA_PATH
+
+class Eng2ChiDset(BaseDset):
+    dset_name = 'eng2chi'
+
+    def __init__(self):
+        super().__init__()
+        df = pd.read_csv(os.path.join(DATA_PATH, 'eng2chi.csv'))
+        self.src.extend(
+            df['src'].apply(str).apply(self.__class__.preprocess).to_list()
+        )
+        self.tgt.extend(
+            df['tgt'].apply(str).apply(self.__class__.preprocess).to_list()
+        )
+
+    @staticmethod
+    def eval(tgt: str, pred: str) -> float:
+        return float(tgt == pred)
+
+    @staticmethod
+    def batch_eval(
+            batch_tgt: Sequence[str],
+            batch_pred: Sequence[str],
+    ) -> float:
+        return accuracy_score(batch_tgt, batch_pred)
